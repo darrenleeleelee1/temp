175171ae0a1b1d278ae8a2ba98e186679a25e15a
darrenleeleelee1 <darrenleeleelee1@gmail.com>
2020-03-13 00:09:40 +0800

    test123

---
+++ src/p1/hi.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("HI I AM DARREN");
+    }
+    public static void HI(String s){
+        System.out.println("Hi" + s);
+    }
+}
---
+++ src/p1/test.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class test {
+    public static void main(String[] args) {
+        hi x = new hi();
+        x.HI("Darren");
+    }
+
+}
